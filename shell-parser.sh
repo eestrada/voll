5041b38f1c18ba82278a041a3cb5e911e0b64d99
Ethan Estrada <ethan.estrada@gmail.com>
2019-03-01 15:24:37 -0700

    Create shell-parser.sh

---
+++ shell-parser.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# It looks like parsing of arrays can be done in plain old POSIX shell, although it takes some work arounds. Those are documented in this array utility:
+# https://github.com/krebs/array
