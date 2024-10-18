ea226e5a884c639d44d139a33c9af9facf81c474
Ethan Estrada <ethan@misterfidget.com>
2024-10-17 22:56:32 -0500

    Begin adding POSIX shell implementation
    Add a simple test file.

--- bin/voll.sh
+++ bin/voll.sh
@@ -1,4 +1,53 @@
 #!/bin/sh
 
-# It looks like parsing of arrays can be done in plain old POSIX shell, although it takes some work arounds. Those are documented in this array utility:
-# https://github.com/krebs/array
+_ws_re='[ \t]*'
+_key_re='[a-zA-Z][a-zA-Z0-9_.]*'
+_safe_key_re='s/\./\\./'
+_main_re='^[ \t]*([a-zA-Z][a-zA-Z0-9_.]*)[ \t]*=(.*)$'
+_comment_re='^[ \t]*(#.*)$'
+_blank_re='^[ \t]*$'
+_list_key_re="s/${_main_re}/\1/"
+_list_value_re="s/${_main_re}/\2/"
+_ltrim_re='s/^[ \t]*//'
+_rtrim_re='s/[ \t]*$//'
+_rtrim_re='s/[ \t]*$//'
+
+voll_filter_blank_lines() {
+    grep -v -E "${_blank_re}"
+}
+
+voll_filter_comment_lines() {
+    grep -v -E "${_comment_re}"
+}
+
+voll_is_key() {
+    _key="$(cat)"
+    if [ -n "${_key}" ] && [ "$(printf '%s' "${_key}" | wc -l)" -le '1' ]; then
+        printf '%s' "${_key}" | grep -E "^${_key_re}$" >/dev/null
+    else
+        return 1
+    fi
+}
+
+voll_list_keys() {
+    sed -E "${_list_key_re}"
+}
+
+voll_list_values() {
+    sed -E "${_list_value_re}"
+}
+
+voll_ltrim_lines() {
+    sed -E "${_ltrim_re}"
+}
+
+voll_rtrim_lines() {
+    sed -E "${_rtrim_re}"
+}
+
+# shellcheck disable=2002
+cat "$1" | voll_filter_comment_lines | voll_filter_blank_lines | voll_list_values
+cat "$1" | voll_filter_comment_lines | voll_filter_blank_lines | voll_list_keys
+
+# printf 'en\nrico\n' | voll_is_key
+echo $?
